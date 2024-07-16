e83ddfaa605fbff4a0b468fc411dd5a4d219542d
Stephane Lacoin (aka nxmatic) <stephane.lacoin@hyland.com>
2024-07-16 19:49:50 +0200

    [nix,gha] nix devenv and github workflow for nuxeo branchesa

---
+++ .devenv/compute-gwt-version.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash -e -o pipefail
+
+process_branch_name() {
+  local BRANCH_NAME=$1
+  if [[ $BRANCH_NAME =~ ^[0-9]+\.[0-9]+\.[0-9]+-nuxeo$ ]]; then
+    echo "${BRANCH_NAME}-beta"
+  else
+    echo "Unable to determine version from branch name: $BRANCH_NAME" >&2
+    return 1
+  fi
+}
+
+if [ -n "$GITHUB_REF" ]; then
+  case $GITHUB_REF in
+    refs/tags/*)
+      echo "${GITHUB_REF#refs/tags/}"
+      ;;
+    refs/heads/*)
+      process_branch_name "${GITHUB_REF#refs/heads/}"
+      ;;
+    *)
+      echo "Unsupported GITHUB_REF: $GITHUB_REF" >&2
+      exit 1
+      ;;
+  esac
+else
+  if TAG=$(git describe --exact-match --tags HEAD 2>/dev/null); then
+    echo "$TAG"
+  else
+    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+    process_branch_name "$BRANCH_NAME"
+  fi
+fi
